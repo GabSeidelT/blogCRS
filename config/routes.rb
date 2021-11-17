e27e8793820b92feb78e4cfff2f04c4815f684c7
Moscow002 <gab.seidel.t@gmail.com>
2021-11-17 18:24:22 -0300

    Devise instalado

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'home#index'
   resources :categories
   resources :roles
+  devise_for :admins
+  devise_for :users, controllers: { sessions: 'users/sessions' }
 end
