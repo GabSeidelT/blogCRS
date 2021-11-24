ef7137aeb2e43c7375315c540d6d294fd1215b0c
Moscow002 <gab.seidel.t@gmail.com>
2021-11-24 18:27:51 -0300

    cancancan 2

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,13 +2,26 @@
 
 class Ability
   include CanCan::Ability
+  
 
-  def initialize(user)
-    if user.admin?
-      can :manage, :all
-    else
-      can [:read, :update, :destroy], Post
-      can :manage, User
-    end
-  end
+  # def initialize(user)
+  #   # if user.admin?
+  #   #   can :manage, :all
+  #   # else
+  #   #   can [:read, :update, :destroy], Car
+  #   #   can :manage, User
+  #   # end
+  # #   if user.admin?
+  # #     can :manage, :all
+  # #   else
+  # #     can :read, Post
+  # #     can :manage, User
+  # #   end
+
+  # #   if user.comment?
+  # #     can [:read, :update, :destroy], Comment
+  # #   end
+
+  # end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,29 @@ class Post < ApplicationRecord
 
   validates_presence_of :name
   validates_presence_of :summary
+  validates_presence_of :content
+
+  # validates :image, presence: true, blob: { content_type: ['image/jpg', 'image/jpeg', 'image/png'], size_range: 1..3.megabytes }
 
   validates_length_of :name, maximum: 16
   validates_length_of :summary, maximum: 255
+  
+  validate :image_type
+
+
+  private
+
+  def image_type
+    if image.attached? == false
+      errors.add(:image, "is missing!")
+    end
+    if image.attached? == true
+      if image.blob.byte_size > 3000000
+        errors.add(:image, 'is too big bro')
+      end
+      if !image.content_type.in?(%('image/jpeg image/png'))
+        errors.add(:image, "needs to be a jpeg or png!")
+      end
+    end
+  end
 end
